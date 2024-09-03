3c5e469980347652ef58402025c354a288cd09a2
anxmarks <anamarks28@outlook.com>
2024-09-03 14:58:52 -0300

    text and icons

--- app/main.py
+++ app/main.py
@@ -1,15 +1,62 @@
 import flet as ft
 from flet import *
 
-BG = '#F5E0B7'
+
 
 #color pallet: E09F7D, EF5D60, 553D36, F9FBB2, CEEC97
 
-page_1 = Container()
-page_2 = Row()
-
-
 def main(page: ft.Page):
+    #colors
+    BG = '#F5E0B7'
+
+    first_page_contests = Container(
+        content=Column(
+            controls=[
+                Row(alignment = 'SpaceBetween',
+                    controls=[
+                        Container(content=Icon(
+                            icons.MENU)),
+                        Row(
+                            controls=[
+                                Icon(icons.SEARCH),
+                                Icon(icons.NOTIFICATIONS_OUTLINED)
+                            ]
+                        )
+                    ]
+                ),
+                Container(height=20), 
+                Text(
+                    value='Welcome!',
+                    color='#553D36'
+                ),
+                Text(
+                    value='CATEGORIES',
+                    color='#553D36'
+                ),
+                Container(
+                    padding=padding.only(top=10, bottom=20)
+                )
+            ]
+        )
+    )
+    page_1 = Container()
+    page_2 = Row(
+        controls=[
+            Container(
+                width=400,
+                height=850,
+                bgcolor='E09F7D',
+                border_radius=35,
+                padding=padding.only(top=50, left=20, right=20, bottom=5),
+                content=Column(
+                    controls=[
+                        first_page_contests
+                    ]
+                )
+
+            )
+        ]
+    )
     container = Container(
         width=400, 
         height=850,
