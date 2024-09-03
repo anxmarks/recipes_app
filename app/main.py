d6360b1a74f1a3b68d4d7b62cb27b34751c35d94
anxmarks <anamarks28@outlook.com>
2024-09-03 13:44:03 -0300

    main container

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 import flet as ft
+from flet import *
+
+BG = '#F5E0B7'
+
+#color pallet: E09F7D, EF5D60, 553D36, F9FBB2, CEEC97
+
+page_1 = Container()
+page_2 = Row()
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    container = Container(
+        width=400, 
+        height=850,
+        bgcolor=BG,
+        border_radius=35,
+        content=Stack(
+            controls=[
+                page_1,
+                page_2
+
+            ]
+        )
+    )
+    page.add(container)
 
 
 ft.app(main)
